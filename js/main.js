7df6b2feafbe23d49bb23d46d5137c856188afe7
Youssef-Muhamed <ym28013@gmail.com>
2022-07-22 22:36:08 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
